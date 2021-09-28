e41e4e0363a43cffedfc6882a4a85cbc4b19e478
Mateusz <mateusz.gorniak@student.wat.edu.pl>
2021-09-28 22:15:36 +0200

    Initialize basic activities

---
+++ app/src/main/java/pl/gorniak/audiogram2_0/LoginActivity.java
@@ -0,0 +1,14 @@
+package pl.gorniak.audiogram2_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/pl/gorniak/audiogram2_0/MainActivity.java
+++ app/src/main/java/pl/gorniak/audiogram2_0/MainActivity.java
@@ -2,7 +2,11 @@ package pl.gorniak.audiogram2_0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void onClickRegisterButton(View view) {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickLoginButton(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
