62876474708a07c36b87e643798bb225aac1a46d
Mateusz <mateusz.gorniak@student.wat.edu.pl>
2021-10-06 20:59:13 +0200

    New features

---
+++ app/src/main/java/pl/gorniak/audiogram2_0/AboutAppActivity.java
@@ -0,0 +1,14 @@
+package pl.gorniak.audiogram2_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
