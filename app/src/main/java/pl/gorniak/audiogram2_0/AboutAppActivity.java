46adf55928c710e4a2957170ec2d169b4e6bde97
Pawel Slapczynski <pawel.slapczynski@student.wat.edu.pl>
2021-10-13 21:33:59 +0200

    zrobienie AboutAppActivity

--- app/src/main/java/pl/gorniak/audiogram2_0/AboutAppActivity.java
+++ app/src/main/java/pl/gorniak/audiogram2_0/AboutAppActivity.java
@@ -2,7 +2,9 @@ package pl.gorniak.audiogram2_0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutAppActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AboutAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+    }
+
+    public void backToMainPage(View view) {
+        Intent backToMainPage = new Intent(this, MainActivity.class);
+        startActivity(backToMainPage);
     }
 }
